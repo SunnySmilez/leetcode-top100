be9aad3cc368054dbfb294d97b31bedc3786f0e5
zhouzhi <zhouzhi@100tal.com>
2021-09-25 00:22:51 +0800

    归并排序测试用例添加

---
+++ test/sort_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	"algo/algo/sort"
+	"reflect"
+	"testing"
+)
+
+type params struct {
+	Params1 []int
+	Params2 []int
+	Result  []int
+}
+
+func TestMergeSort(t *testing.T) {
+	var allParams []params
+	test1 := params{
+		Params1: []int{0},
+		Params2: []int{5},
+		Result:  []int{0, 5},
+	}
+
+	test2 := params{
+		Params1: []int{1, 3, 5},
+		Params2: []int{4, 5, 6},
+		Result:  []int{1, 3, 4, 5, 5, 6},
+	}
+
+	allTest := append(append(allParams, test1), test2)
+
+	for _, v := range allTest {
+		res := sort.MergeSort(v.Params1, v.Params2)
+
+		if reflect.DeepEqual(res, v.Result) == false {
+			t.Error("Expected:", v.Result, ",Got:", res)
+		}
+	}
+}
