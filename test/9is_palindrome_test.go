5a26831ee1183ee74e22e04a114632252673a5b7
zhouzhi <zhouzhi@100tal.com>
2021-09-30 10:39:48 +0800

    9.是否是回文串

---
+++ test/9is_palindrome_test.go
@@ -0,0 +1,62 @@
+package test
+
+import (
+	"algo/algo"
+	"fmt"
+	"testing"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	type params struct {
+		Params int
+		Result bool
+	}
+
+	testP1 := params{
+		Params: 121,
+		Result: true,
+	}
+
+	testP2 := params{
+		Params: -121,
+		Result: false,
+	}
+
+	testP3 := params{
+		Params: 10,
+		Result: false,
+	}
+
+	testP4 := params{
+		Params: -101,
+		Result: false,
+	}
+
+	testP5 := params{
+		Params: 123,
+		Result: false,
+	}
+	testArr := []params{testP1, testP2, testP3, testP4, testP5}
+
+	for k, v := range testArr {
+		res := algo.IsPalindrome(v.Params)
+		if res != v.Result {
+			t.Errorf("Case:%d,params: %d,Expected:%t,Got:%t", k, v.Params, v.Result, res)
+		}
+	}
+}
+
+func TestSingle1(t *testing.T) {
+	type params struct {
+		Params int
+		Result bool
+	}
+
+	testP5 := params{
+		Params: 123,
+		Result: false,
+	}
+
+	res := algo.IsPalindrome(testP5.Params)
+	fmt.Print(res)
+}
