5cc8282012d9a0a0a86e51a5bcd7aa38c3920515
zhouzhi <phpchengxuyuan@sina.com>
2021-10-19 23:03:23 +0800

    最长子串o(n)解法

--- algo/hot100/52maximumSubarray.go
+++ algo/hot100/52maximumSubarray.go
@@ -68,3 +68,19 @@ func MaxSubArray(nums []int) int {
 
 	return maxNum
 }
+
+func MaxSubArr(nums []int) int {
+	max := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+
+	return max
+}
