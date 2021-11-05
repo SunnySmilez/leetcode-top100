07eaebeaabb5a8f93bbc3312179c4ea394b9239d
zhouzhi <zhouzhi@100tal.com>
2021-11-05 17:12:10 +0800

    1. 二叉树的直径

---
+++ algo/hot100/543diameterOfBinaryTree.go
@@ -0,0 +1,47 @@
+package hot100
+
+import (
+	"algo/algo/common"
+	"fmt"
+)
+
+/*
+543. 二叉树的直径:https://leetcode-cn.com/problems/diameter-of-binary-tree/
+给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+示例 :
+给定二叉树
+          1
+         / \
+        2   3
+       / \
+      4   5
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+*/
+var ans int
+
+func DiameterOfBinaryTree(root *common.TreeNode) int {
+	a := travel461(root)
+
+	fmt.Printf("ans:%d", ans, "max:%d", a)
+	return ans
+}
+
+func travel461(root *common.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	L := travel461(root.Left)
+	R := travel461(root.Right)
+
+	ans = max(L+R, ans)
+	return max(L, R) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
