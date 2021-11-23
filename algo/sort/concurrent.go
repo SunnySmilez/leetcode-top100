6ebf0db162f83e62ccc76551359aac434bb877c1
zhouzhi <zhouzhi@100tal.com>
2021-11-23 18:27:42 +0800

    1. go concurrent 2. 快排

---
+++ algo/sort/concurrent.go
@@ -0,0 +1,56 @@
+package sort
+
+import (
+	"sync"
+)
+
+func Concurrent() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	a := make(chan bool)
+	b := make(chan bool)
+	c := make(chan bool, 1)
+
+	c <- true
+
+	go printA(&wg, c, a)
+	go printB(&wg, a, b)
+	go printC(&wg, b, c)
+
+	wg.Wait()
+
+}
+
+func printA(wg *sync.WaitGroup, c chan bool, a chan bool) {
+	for i := 0; i < 3; i++ {
+		if <-c == true {
+			print("A")
+			a <- true
+		}
+	}
+
+	wg.Done()
+}
+
+func printB(wg *sync.WaitGroup, a chan bool, b chan bool) {
+	for i := 0; i < 3; i++ {
+		if <-a == true {
+			print("B")
+			b <- true
+		}
+	}
+
+	wg.Done()
+}
+
+func printC(wg *sync.WaitGroup, b chan bool, c chan bool) {
+	for i := 0; i < 3; i++ {
+		if <-b == true {
+			print("C")
+			c <- true
+		}
+	}
+
+	wg.Done()
+}
--- algo/sort/quick.go
+++ algo/sort/quick.go
@@ -1,5 +1,33 @@
 package sort
 
+/**
+ * 快速排序
+ * https://www.jianshu.com/p/1bd09e10f6db
+ * 思路：选择一个基准元素，通常选择第一个元素或者最后一个元素。
+ *      通过一趟扫描，将待排序列分成两部分，一部分比基准元素小，一部分大于等于基准元素。
+ *      此时基准元素在其排好序后的正确位置，然后再用同样的方法递归地排序划分的两部分
+ * @param $params
+ * @return mixed
+ */
 func Quick(nums []int) (res []int) {
-	return
+	var left []int
+	var right []int
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[0] {
+			right = append(right, nums[i])
+		} else {
+			left = append(left, nums[i])
+		}
+	}
+
+	left = Quick(left)
+	right = Quick(right)
+
+	left = append(left, nums[0])
+
+	return MergeSort(left, right)
 }
